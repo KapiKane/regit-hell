10636837771eb3107cd762c9150ebca3f6603757
John <Lailo_Sux.Still@proton.me>
2024-11-08 02:41:33 -0700

    Debugged UntestedTUI.cmd

--- TrustedInstaller/UntestedTUI.cmd
+++ TrustedInstaller/UntestedTUI.cmd
@@ -1,22 +1,23 @@
 @echo off
 title TrustedInstaller
 
-echo " ______   ______     __  __     ______     ______   ______     _____     __     __   __     ______     ______   ______     __         __         ______     ______    "
-echo "/\__  _\ /\  == \   /\ \/\ \   /\  ___\   /\__  _\ /\  ___\   /\  __-.  /\ \   /\ "-.\ \   /\  ___\   /\__  _\ /\  __ \   /\ \       /\ \       /\  ___\   /\  == \   "
-echo "\/_/\ \/ \ \  __<   \ \ \_\ \  \ \___  \  \/_/\ \/ \ \  __\   \ \ \/\ \ \ \ \  \ \ \-.  \  \ \___  \  \/_/\ \/ \ \  __ \  \ \ \____  \ \ \____  \ \  __\   \ \  __<   "
-echo "   \ \_\  \ \_\ \_\  \ \_____\  \/\_____\    \ \_\  \ \_____\  \ \____-  \ \_\  \ \_\\"\_\  \/\_____\    \ \_\  \ \_\ \_\  \ \_____\  \ \_____\  \ \_____\  \ \_\ \_\ "
-echo "    \/_/   \/_/ /_/   \/_____/   \/_____/     \/_/   \/_____/   \/____/   \/_/   \/_/ \/_/   \/_____/     \/_/   \/_/\/_/   \/_____/   \/_____/   \/_____/   \/_/ /_/ "
-                                                                                                                                                                      
-  set %enable% /p Do you want to enable trusted installer? (Y/n): 
+echo  ______   ______     __  __     ______     ______   ______     _____     __     __   __     ______     ______   ______     __         __         ______     ______    
+echo /\__  _\ /\  == \   /\ \/\ \   /\  ___\   /\__  _\ /\  ___\   /\  __-.  /\ \   /\ "-.\ \   /\  ___\   /\__  _\ /\  __ \   /\ \       /\ \       /\  ___\   /\  == \   
+echo \/_/\ \/ \ \  __<   \ \ \_\ \  \ \___  \  \/_/\ \/ \ \  __\   \ \ \/\ \ \ \ \  \ \ \-.  \  \ \___  \  \/_/\ \/ \ \  __ \  \ \ \____  \ \ \____  \ \  __\   \ \  __<   
+echo    \ \_\  \ \_\ \_\  \ \_____\  \/\_____\    \ \_\  \ \_____\  \ \____-  \ \_\  \ \_\\"\_\  \/\_____\    \ \_\  \ \_\ \_\  \ \_____\  \ \_____\  \ \_____\  \ \_\ \_\ 
+echo     \/_/   \/_/ /_/   \/_____/   \/_____/     \/_/   \/_____/   \/____/   \/_/   \/_/ \/_/   \/_____/     \/_/   \/_/\/_/   \/_____/   \/_____/   \/_____/   \/_/ /_/ 
 
-  :a
-  If %enable% == Y && y (
-  run EnableTrustedInstaller.regedit
-  )
-  elif %enable% == n && N (
-  run DisableTrustedInstaller.regedit
-  ) else (
-  echo "No proper input try again"
-  pause
-  goto a
-  )
+set /p enable="Do you want to enable TrustedInstaller? (Y/n): "
+
+:a
+if /I "%enable%"=="Y" (
+    start EnableTrustedInstaller.reg
+    echo "TrustedInstaller enabled."
+) else if /I "%enable%"=="N" (
+    start DisableTrustedInstaller.reg
+    echo "TrustedInstaller disabled."
+) else (
+    echo "No proper input. Try again."
+    set /p enable="Do you want to enable TrustedInstaller? (Y/n): "
+    goto a
+)
