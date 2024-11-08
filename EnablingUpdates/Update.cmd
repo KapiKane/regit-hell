2be891fece7ec5f651a9d3b9154f4142434231d5
John <Lailo_Sux.Still@proton.me>
2024-11-08 16:58:38 -0700

    Update Update.cmd

--- EnablingUpdates/Update.cmd
+++ EnablingUpdates/Update.cmd
@@ -1,1 +1,24 @@
-echo ugh not doing that (yet ;))
+@echo off
+title TrustedInstaller
+
+echo  __  __     ______   _____     ______     ______   ______     ______    
+echo /\ \/\ \   /\  == \ /\  __-.  /\  __ \   /\__  _\ /\  ___\   /\  == \   
+echo \ \ \_\ \  \ \  _-/ \ \ \/\ \ \ \  __ \  \/_/\ \/ \ \  __\   \ \  __<   
+echo  \ \_____\  \ \_\    \ \____-  \ \_\ \_\    \ \_\  \ \_____\  \ \_\ \_\ 
+echo   \/_____/   \/_/     \/____/   \/_/\/_/     \/_/   \/_____/   \/_/ /_/ 
+                                                                         
+
+set /p enable="Do you want to enable Updates?? (Y/n): "
+
+:a
+if /I "%enable%"=="Y" || "%enable%"=="y" (
+    start EnablingUpdates/Enable Updates.reg
+    echo "TrustedInstaller enabled."
+) else if /I "%enable%"=="N" || "%enable%"=="n" (
+    start EnablingUpdates/Disable Updates.reg
+    echo "TrustedInstaller disabled."
+) else (
+    echo "No proper input. Try again."
+    set /p enable="Do you want to enable TrustedInstaller? (Y/n): "
+    goto a
+)
